267a71f5f395395a789b0adfa3716ee9e0db16d4
Debopriya Roy <dpriya.jsr@gmail.com>
2021-08-21 13:49:31 +0530

    Added Prop in User Model

--- src/Database/Users/users.model.ts
+++ src/Database/Users/users.model.ts
@@ -1,12 +1,27 @@
 import * as mongoose from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
-export const UserSchema = new mongoose.Schema<User>({
-    username: { type : String, required: true, unique: true },
-    password: { type : String, required: true }
-});
+// export const UserSchema = new mongoose.Schema<User>({
+//     username: { type : String, required: true, unique: true },
+//     password: { type : String, required: true }
+// });
 
-export interface User extends mongoose.Document {
-    id: string,
-    username: string,
-    password: string,
+// export interface User extends mongoose.Document {
+//     id: string,
+//     username: string,
+//     password: string,
+// }
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+    @Prop()
+    id: string;
+    @Prop()
+    username: string;
+    @Prop()
+    password: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
