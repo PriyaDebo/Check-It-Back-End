730080e9a06ce5396182261f2d9d4a5d72ca5952
PriyaDebo <73054296+PriyaDebo@users.noreply.github.com>
2021-08-21 13:53:29 +0530

    Add prop (#2)
    * Added Authentication
    
    * Added Prop in User Model

--- src/Database/Users/users.model.ts
+++ src/Database/Users/users.model.ts
@@ -1,12 +1,17 @@
 import * as mongoose from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
-export const UserSchema = new mongoose.Schema<User>({
-    username: { type : String, required: true, unique: true },
-    password: { type : String, required: true }
-});
+export type UserDocument = User & Document;
 
-export interface User extends mongoose.Document {
-    id: string,
-    username: string,
-    password: string,
+@Schema()
+export class User {
+    @Prop()
+    id: string;
+    @Prop()
+    username: string;
+    @Prop()
+    password: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
